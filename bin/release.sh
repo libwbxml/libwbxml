17d7018c69b5324cb662d4338e8629c3d79a0ed3
Aymerick Jehanne <aymerick@jehanne.org>
2006-07-10 10:24:52 +0000

    Release maker, inspired from Expat project
    git-svn-id: http://svn.opensync.org/libwbxml/wbxml2/trunk@19 86abe414-4816-0410-bace-a393d2798175

---
+++ bin/release.sh
@@ -0,0 +1,65 @@
+#! /bin/bash
+#
+# release.sh: make a libwbxml release
+#
+# USAGE: release.sh tagname
+#
+# Note: tagname may be HEAD to just grab the head revision
+#
+
+SVNROOT='http://libwbxml.aymerick.com:8080/repo/wbxml2/trunk'
+
+if test $# != 1; then
+  echo "USAGE: $0 tagname"
+  exit 1
+fi
+
+tmpdir=wbxml2-release.$$
+if test -e $tmpdir; then
+  echo "ERROR: oops. chose the $tmpdir subdir, but it exists."
+  exit 1
+fi
+
+echo "Checking out into temporary area: $tmpdir"
+svn co -q -r "$1" "$SVNROOT" $tmpdir || exit 1
+
+# figure out the release version
+vsn="`$tmpdir/version.sh $tmpdir/src/wbxml.h`"
+
+echo ""
+echo "Release version: $vsn"
+
+if test "$1" = HEAD ; then
+  distdir=wbxml2-`date '+%Y-%m-%d'`
+else
+  distdir=wbxml2-$vsn
+fi
+
+if test -e $distdir; then
+  echo "ERROR: for safety, you must manually remove $distdir."
+  rm -rf $tmpdir
+  exit 1
+fi
+
+mkdir $distdir || exit 1
+
+echo ""
+echo "----------------------------------------------------------------------"
+echo "Building $distdir based on the MANIFEST:"
+files="`sed -e 's/[ ]:.*$//' $tmpdir/MANIFEST`"
+for file in $files; do
+  echo "Copying $file..."
+  (cd $tmpdir && cp $CPOPTS $file ../$distdir) || exit 1
+done
+
+echo ""
+echo "----------------------------------------------------------------------"
+echo "Removing (temporary) checkout directory..."
+rm -rf $tmpdir
+
+tarball=$distdir.tar.gz
+echo "Constructing $tarball..."
+tar cf - $distdir | gzip -9 > $tarball || exit $?
+rm -r $distdir
+
+echo "Done."
---
+++ bin/version.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#
+# USAGE: version.sh path/to/wbxml.h
+#
+# This script will print libwbxml version number on stdout. For example:
+#
+#   $ ./version.sh ../src/wbxml.h
+#   0.9.2
+#   $
+#
+
+if test $# = 0; then
+  echo "ERROR: pathname for wbxml.h was not provided."
+  echo ""
+  echo "USAGE: $0 path/to/wbxml.h"
+  exit 1
+fi
+
+if test $# != 1; then
+  echo "ERROR: too many arguments were provided."
+  echo ""
+  echo "USAGE: $0 path/to/wbxml.h"
+  exit 1
+fi
+
+hdr="$1"
+if test ! -r "$hdr"; then
+  echo "ERROR: '$hdr' does not exist, or is not readable."
+  exit 1
+fi
+
+WBXML_VERSION="`sed -n -e '/WBXML_LIB_VERSION/s/[^0-9.]*//gp' $hdr`"
+
+# Determine how to tell echo not to print the trailing \n. This is
+# similar to Autoconf's @ECHO_C@ and @ECHO_N@;
+case `echo "testing\c"; echo 1,2,3`,`echo -n testing; echo 1,2,3` in
+  *c*,-n*) ECHO_N= ECHO_C='
+' ;;
+  *c*,*  ) ECHO_N=-n ECHO_C= ;;
+  *)      ECHO_N= ECHO_C='\c' ;;
+esac
+
+echo $ECHO_N "$WBXML_VERSION$ECHO_C"
