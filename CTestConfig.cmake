b7bb859833fa2c9eaa35c9240ff7fb37764ffb2a
Daniel Gollub <daniel.gollub@gmail.com>
2008-11-25 15:09:30 +0000

    CDash configuration for opensync.org/testing cdash infra.
    git-svn-id: http://svn.opensync.org/libwbxml/wbxml2/trunk@97 86abe414-4816-0410-bace-a393d2798175

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "libwbxml")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 CST")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "opensync.org")
+set(CTEST_DROP_LOCATION "/testing/submit.php?project=libwbxml")
+set(CTEST_DROP_SITE_CDASH TRUE)
