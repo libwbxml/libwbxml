27041da786e5e583d94883a659bb764f8bd517dc
Michael Bell <michael.bell@web.de>
2008-11-28 13:44:49 +0000

    fixed exported wbxml configuration
    git-svn-id: http://svn.opensync.org/libwbxml/wbxml2/trunk@112 86abe414-4816-0410-bace-a393d2798175

---
+++ src/wbxml_config.h.cmake
@@ -0,0 +1,23 @@
+/* config.h.cmake */
+
+/* Define to 1 if you have the `expat' library (-lexpat). */
+#cmakedefine HAVE_EXPAT
+
+/* Define to 1 if you would like to enable debug, warning and error messages */
+#cmakedefine WBXML_LIB_VERBOSE
+
+/* supported document types */
+#cmakedefine WBXML_ENCODER_USE_STRTBL
+#cmakedefine WBXML_SUPPORT_WML
+#cmakedefine WBXML_SUPPORT_WTA
+#cmakedefine WBXML_SUPPORT_SI
+#cmakedefine WBXML_SUPPORT_SL
+#cmakedefine WBXML_SUPPORT_CO
+#cmakedefine WBXML_SUPPORT_PROV
+#cmakedefine WBXML_SUPPORT_EMN
+#cmakedefine WBXML_SUPPORT_DRMREL
+#cmakedefine WBXML_SUPPORT_OTA_SETTINGS
+#cmakedefine WBXML_SUPPORT_SYNCML
+#cmakedefine WBXML_SUPPORT_WV
+#cmakedefine WBXML_SUPPORT_AIRSYNC
+
