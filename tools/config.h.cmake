53c77267fd49b964f3180e8e865d765433b45aca
Michael Bell <michael.bell@web.de>
2009-05-11 12:29:59 +0000

    added forgotten file of commit r213
    git-svn-id: http://svn.opensync.org/libwbxml/wbxml2/trunk@214 86abe414-4816-0410-bace-a393d2798175

---
+++ tools/config.h.cmake
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2008 Michael Bell <michael.bell@opensync.org>
+ */
+
+#ifndef WBXML_TOOLS_CONFIG_H
+#define WBXML_TOOLS_CONFIG_H
+
+#cmakedefine FOUND_POSIX_GETOPT
+
+#endif /* WBXML_TOOLS_CONFIG_H */
