7507538502bf8ffa5bd44e3ef547af0ce3de53ae
Michael Bell <michael.bell@web.de>
2011-01-20 17:35:08 +0000

    copyright update
    git-svn-id: http://svn.opensync.org/libwbxml/wbxml2/trunk@281 86abe414-4816-0410-bace-a393d2798175

--- src/wbxml_tables.h
+++ src/wbxml_tables.h
@@ -1,7 +1,7 @@
 /*
  * libwbxml, the WBXML Library.
  * Copyright (C) 2002-2008 Aymerick Jehanne <aymerick@jehanne.org>
- * Copyright (C) 2009 Michael Bell <michael.bell@opensync.org>
+ * Copyright (C) 2009-2011 Michael Bell <michael.bell@opensync.org>
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
