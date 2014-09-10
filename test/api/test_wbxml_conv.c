2b61d33020270feba7e61ffc926b504c9fbc9853
Michael Bell <michael.bell@web.de>
2014-09-10 08:40:17 +0200

    Added check for wrong creation of converters.

---
+++ test/api/test_wbxml_conv.c
@@ -0,0 +1,18 @@
+#include "api_test.h"
+
+#include "../../src/wbxml_conv.h"
+
+START_TEST (security_test_conv_init_null_reference)
+{
+    /* use undefined converter address reference */
+    ck_assert(wbxml_conv_wbxml2xml_create(NULL) == WBXML_ERROR_BAD_PARAMETER);
+    ck_assert(wbxml_conv_xml2wbxml_create(NULL) == WBXML_ERROR_BAD_PARAMETER);
+}
+END_TEST
+
+BEGIN_TESTS(wbxml_encoder_internals)
+
+    ADD_TEST(security_test_conv_init_null_reference);
+
+END_TESTS
+
