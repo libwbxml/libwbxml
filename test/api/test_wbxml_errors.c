27a08ba4aa9396b37882a6fccd2cee09d9fbb91b
Michael Bell <michael.bell@web.de>
2015-11-24 00:17:00 +0100

    Fixed MacOS X compiler warnings for error tests.

--- test/api/test_wbxml_errors.c
+++ test/api/test_wbxml_errors.c
@@ -4,31 +4,31 @@
 
 START_TEST (security_test_errors_datetime_without_percent)
 {
-    ck_assert(strchr(wbxml_errors_string(WBXML_ERROR_BAD_DATETIME), '%') == NULL);
+    ck_assert(WBXML_STRSTR(wbxml_errors_string(WBXML_ERROR_BAD_DATETIME), "%") == NULL);
 }
 END_TEST
 
 START_TEST (test_errors_ok)
 {
     ck_assert(wbxml_errors_string(WBXML_OK) != NULL);
-    ck_assert(strcmp(wbxml_errors_string(WBXML_OK), "Unknown Error Code") != 0);
+    ck_assert(WBXML_STRCMP(wbxml_errors_string(WBXML_OK), "Unknown Error Code") != 0);
 }
 END_TEST
 
 START_TEST (test_errors_last_item)
 {
     ck_assert(wbxml_errors_string(WBXML_ERROR_XMLPARSER_OUTPUT_UTF16) != NULL);
-    ck_assert(strcmp(wbxml_errors_string(WBXML_ERROR_XMLPARSER_OUTPUT_UTF16), "Unknown Error Code") != 0);
+    ck_assert(WBXML_STRCMP(wbxml_errors_string(WBXML_ERROR_XMLPARSER_OUTPUT_UTF16), "Unknown Error Code") != 0);
 
     ck_assert(wbxml_errors_string(WBXML_ERROR_CHARSET_NOT_FOUND) != NULL);
-    ck_assert(strcmp(wbxml_errors_string(WBXML_ERROR_CHARSET_NOT_FOUND), "Unknown Error Code") != 0);
+    ck_assert(WBXML_STRCMP(wbxml_errors_string(WBXML_ERROR_CHARSET_NOT_FOUND), "Unknown Error Code") != 0);
 }
 END_TEST
 
 START_TEST (test_errors_unknown_error_code)
 {
     ck_assert(wbxml_errors_string(127) != NULL);
-    ck_assert(strcmp(wbxml_errors_string(127), "Unknown Error Code") == 0);
+    ck_assert(WBXML_STRCMP(wbxml_errors_string(127), "Unknown Error Code") == 0);
 }
 END_TEST
 
